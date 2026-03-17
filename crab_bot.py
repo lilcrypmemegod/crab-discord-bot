786ad45308b9202ab7271ffeec819abb510edb63
lilcrypmemegod <crisstheart77@gmail.com>
2026-03-17 11:12:47 -0700

    Update crab_bot.py

--- crab_bot.py
+++ crab_bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import requests
+import random
 
 TOKEN = os.getenv("TOKEN")
 
@@ -11,6 +12,13 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 PAIR_API = "https://api.dexscreener.com/latest/dex/pairs/cronos/0xdf9030e28cde0f4e6f11c65362c5e152093c7414"
+
+crab_gifs = [
+    "https://media.giphy.com/media/Y4nmq4GOnLf4XIgALX/giphy.gif",
+    "https://media.giphy.com/media/kGdP6qyP3xTS2efTW0/giphy.gif",
+    "https://media.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif",
+    "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif"
+]
 
 @bot.event
 async def on_ready():
@@ -23,7 +31,6 @@ async def crab(ctx):
         data = r.json()
 
         pair = data["pair"]
-
         mc = pair.get("marketCap") or pair.get("fdv")
 
         if mc >= 1_000_000:
@@ -33,7 +40,9 @@ async def crab(ctx):
         else:
             mc_text = f"${mc}"
 
-        await ctx.send(f"MC - {mc_text}")
+        gif = random.choice(crab_gifs)
+
+        await ctx.send(f"🦀🔪 **CRAB WITH KNIFE**\nMC - {mc_text}\n{gif}")
 
     except Exception as e:
         print(e)
