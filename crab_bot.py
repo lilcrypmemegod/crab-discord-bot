cec33dde9fe7a809afe80def2395ec86ac8226a1
lilcrypmemegod <crisstheart77@gmail.com>
2026-03-17 08:31:30 -0700

    Update crab_bot.py

--- crab_bot.py
+++ crab_bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-import random
 
 TOKEN = os.getenv("TOKEN")
 
@@ -10,26 +9,12 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-crab_memes = [
-    "🦀 CRAB WITH KNIFE",
-    "🦀 SNIP SNIP",
-    "🦀 CRAB MODE ACTIVATED",
-]
-
 @bot.event
 async def on_ready():
     print(f"Bot connected as {bot.user}")
 
-@bot.command()
+@bot.command(name="crab")
 async def crab(ctx):
-    await ctx.send(random.choice(crab_memes))
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("hello 🦀")
-
-@bot.command()
-async def price(ctx):
-    await ctx.send("CRAB price coming soon 🦀")
+    await ctx.send("MC - $6.5M")
 
 bot.run(TOKEN)
