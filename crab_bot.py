3b7c0585c4a50ba6a3556cc30bebaeb7515f7c65
lilcrypmemegod <crisstheart77@gmail.com>
2026-03-17 14:54:39 -0700

    Update crab_bot.py

--- crab_bot.py
+++ crab_bot.py
@@ -13,11 +13,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 MC = "$35.56K"
 
 crab_gifs = [
-"https://media.tenor.com/8gZ5vH1K6VMAAAAd/licking-knife-crabby.gif",
-"https://media.tenor.com/bx5b3hR9Y2EAAAAd/crab-with-a-knife.gif",
-"https://media.tenor.com/Fp7E9n4s2FMAAAAd/crab-stabby.gif",
-"https://media.tenor.com/fxL2X4S6zM0AAAAd/man-crab-knife.gif",
-"https://media.tenor.com/7M4F2mP0k6YAAAAd/crab-knife-fight.gif"
+"https://media.tenor.com/images/6c9d1b2d3c8dbe5285bdcf0584d29c0e/tenor.gif",
+"https://media.tenor.com/images/8d9b48c7a07f9dcbfcba1cc403a53d58/tenor.gif",
+"https://media.tenor.com/images/1f21d71f7d23d5d7d64f3f7e5f6e4f3c/tenor.gif",
+"https://media.tenor.com/images/9b3f9e7b1d0c3a7c2b6c0bba2d3b77c0/tenor.gif",
+"https://media.tenor.com/images/cfb94f7f8e5f19c6e54c90c6e2c5e9fb/tenor.gif"
 ]
 
 class CrabButton(discord.ui.View):
@@ -36,10 +36,7 @@ class CrabButton(discord.ui.View):
             f"🚨🦀 **{interaction.user.name} pressed the crab button** 🦀🚨\nMC - {MC}"
         )
 
-        embed = discord.Embed()
-        embed.set_image(url=gif)
-
-        await interaction.followup.send(embed=embed)
+        await interaction.followup.send(gif)
 
         if random.randint(1,777) == 1:
             await interaction.followup.send(
@@ -56,11 +53,7 @@ async def crab(ctx):
     gif = random.choice(crab_gifs)
 
     await ctx.send(f"MC - {MC}")
-
-    embed = discord.Embed()
-    embed.set_image(url=gif)
-
-    await ctx.send(embed=embed)
+    await ctx.send(gif)
 
 @bot.command(name="CRAB")
 async def crab_button(ctx):
